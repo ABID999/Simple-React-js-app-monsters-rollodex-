4e8d5399c75472aa39d5c3543c8e6c50f05a3946
ABID999 <abidislam49@gmail.com>
2020-06-21 21:41:40 +0600

    Updates

---
+++ precache-manifest.307ade981f0e3f3ed3be1dbd6a5666a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4711028eb60920ad448c6da9d03042f0",
+    "url": "/monsters-rollodex/index.html"
+  },
+  {
+    "revision": "ea48b27296dbbc8e18b5",
+    "url": "/monsters-rollodex/static/css/main.c3f4b7c2.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rollodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rollodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea48b27296dbbc8e18b5",
+    "url": "/monsters-rollodex/static/js/main.c073790d.chunk.js"
+  },
+  {
+    "revision": "825085b0275deac5a0d8",
+    "url": "/monsters-rollodex/static/js/runtime-main.3028a632.js"
+  }
+]);
